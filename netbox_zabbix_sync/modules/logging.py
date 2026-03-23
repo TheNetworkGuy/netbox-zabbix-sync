b2dca79a457762f4cf7401d771ddffceb4403da6
Wouter de Bruijn <wouter@hedium.nl>
2026-03-23 10:33:39 +0100

    🐛 Changed log directory to current working dir instead of project root

--- netbox_zabbix_sync/modules/logging.py
+++ netbox_zabbix_sync/modules/logging.py
@@ -21,9 +21,10 @@ def setup_logger():
     """
     # Set logging
     lgout = logging.StreamHandler()
-    # Logfile in the project root
-    project_root = path.dirname(path.dirname(path.realpath(__file__)))
-    logfile_path = path.join(project_root, "sync.log")
+
+    # Create log file in current working directory
+    working_dir = path.realpath(path.curdir)
+    logfile_path = path.join(working_dir, "sync.log")
     lgfile = logging.FileHandler(logfile_path)
 
     logging.basicConfig(
