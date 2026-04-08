8d87458c5c9f17cb23adc992f1ff186ba9fd96e3
Raymond Kuiper <qix@the-wired.net>
2026-04-08 15:14:48 +0200

    :constuction: Add custom Jinja2 filters

--- netbox_zabbix_sync/modules/jinja_filters.py
+++ netbox_zabbix_sync/modules/jinja_filters.py
@@ -1,4 +1,4 @@
-""" Custom Jinja2 filters, these will be loaded by the jinja parsers. """
+"""Custom Jinja2 filters, these will be loaded by the jinja parsers."""
 
 import re
 
@@ -6,21 +6,23 @@ import dateutil
 
 
 def regex_replace(s, find, replace):
-    """ Regex replace """
+    """Regex replace"""
     return re.sub(find, replace, s)
 
+
 def regex_search(s, find):
-    """ 
+    """
     Regex search - Only prints matches
     """
     match = re.search(find, s)
     if match:
-       return match.group()
+        return match.group()
     return ""
 
+
 def strftime(date, fmt=None):
-    """ Format timestrings """
+    """Format timestrings"""
     date = dateutil.parser.parse(date)
     native = date.replace(tzinfo=None)
-    format = fmt or '%Y-%m-%d %H:%M:%S'
+    format = fmt or "%Y-%m-%d %H:%M:%S"
     return native.strftime(format)
