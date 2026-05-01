04ac52f4a3e6bedfb97adef66c337b0caadbba4e
Raymond Kuiper <qix@the-wired.net>
2026-05-01 16:30:55 +0200

    :feature: allow setting ip and dns for main and oob interface from config context

--- netbox_zabbix_sync/modules/virtual_machine.py
+++ netbox_zabbix_sync/modules/virtual_machine.py
@@ -48,7 +48,7 @@ class VirtualMachine(Host):
         zabbix_snmp_interface_type = 2
         try:
             # Initiate interface class
-            interface = ZabbixInterface(self.nb.config_context, self.ip, self.dns)
+            interface = ZabbixInterface(self.nb.config_context, self.ip, self.dns, self.config['prefer_dns'])
             # Check if NetBox has device context.
             # If not fall back to old config.
             if interface.get_context():
